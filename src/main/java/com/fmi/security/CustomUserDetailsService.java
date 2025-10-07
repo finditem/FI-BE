d860f3ccc023d7ff52e88fb942f915e3943463bd
YooSejeong <nived3@naver.com>
2025-10-08 02:25:06 +0900

    feat: email을 통해 회원 객체 가져오는 서비스 코드

--- src/main/java/com/fmi/security/CustomUserDetailsService.java
+++ src/main/java/com/fmi/security/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.fmi.security;
 
 import com.fmi.domain.User;
+import com.fmi.global.apiPayload.code.status.ErrorStatus;
+import com.fmi.global.apiPayload.exception.GeneralException;
 import com.fmi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
@@ -9,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.List;
@@ -30,6 +33,13 @@ public class CustomUserDetailsService implements UserDetailsService {
                 authorities
         );
     }
+
+    @Transactional(readOnly = true)
+    public User findUser(String email) {
+       return userRepository.findByEmail(email).orElseThrow(
+               () -> new GeneralException(ErrorStatus._USER_NOT_FOUND));
+    }
+
 }
 
 
