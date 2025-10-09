9749805c8d427888ee4b45f5be75a8bf47404c7e
YooSejeong <nived3@naver.com>
2025-10-10 01:56:47 +0900

    refactor: Post 엔티티 기본키 필드명 id로 설정

--- src/main/java/com/fmi/domain/post/data/Post.java
+++ src/main/java/com/fmi/domain/post/data/Post.java
@@ -18,8 +18,7 @@ public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "post_id")
-    private Long postId;
+    private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
