645e11d1c838a295028adfe16b67ebfccc0aa3f7
YooSejeong <nived3@naver.com>
2025-10-15 11:52:50 +0900

    feat: 컬럼 수정 및 연관관계 설정

--- src/main/java/com/fmi/domain/chatmessage/data/ChatMessage.java
+++ src/main/java/com/fmi/domain/chatmessage/data/ChatMessage.java
@@ -1,14 +1,20 @@
 package com.fmi.domain.chatmessage.data;
 
 import com.fmi.domain.User;
+import com.fmi.domain.chatmessage.data.enums.MessageType;
+import com.fmi.domain.chatroom.data.ChatRoom;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -17,18 +23,33 @@ public class ChatMessage {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "chatmessage_id")
-    private Long chatmessage_id;
+    private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @Column(name = "content", nullable = false)
+    @Column(name = "content")
     private String content;
 
-    @Column(name = "readornot", nullable = false)
+    //@Column(name = "readornot", nullable = false)
     private boolean readornot;
+
+    @Enumerated(EnumType.STRING)
+    private MessageType messageType;
+
+    @ManyToOne(fetch= FetchType.LAZY)
+    @JoinColumn(name="chatRoom_id", nullable=false)
+    private ChatRoom chatRoom;
+
+    @OneToMany(mappedBy = "chatMessage", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<MessageImage> images = new ArrayList<>();
+
+    public void addImage(MessageImage image) {
+        this.images.add(image);
+        image.setChatMessage(this);
+    }
 
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
