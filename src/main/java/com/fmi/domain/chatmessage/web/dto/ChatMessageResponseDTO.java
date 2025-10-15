d35282d7a48721bb3d074f322191e50537aefb75
YooSejeong <nived3@naver.com>
2025-10-15 11:56:28 +0900

    feat: 사진 포함 채팅 전송, 텍스트만 채팅 전송 기능 추가

---
+++ src/main/java/com/fmi/domain/chatmessage/web/dto/ChatMessageResponseDTO.java
@@ -0,0 +1,41 @@
+package com.fmi.domain.chatmessage.web.dto;
+
+import com.fmi.domain.chatmessage.data.enums.MessageType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ChatMessageResponseDTO {
+
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class MessageResponseDTO {
+        private Long messageId;
+        private Long roomId;
+        private Long senderId;
+        private String content;
+        private MessageType messageType;
+        private LocalDateTime createdAt;
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class MessageImageResponseDTO {
+        private Long messageId;
+        private Long roomId;
+        private Long senderId;
+        private String content;
+        private MessageType messageType;
+        private List<String> imageUrls;
+        private LocalDateTime createdAt;
+    }
+
+}
