d87febeb45326b85830749f049c19e9f50e7dbdb
wook099 <codnrud99@naver.com>
2025-11-22 18:29:59 +0900

    feat: 게시글 생성 : 카테고리 추가

--- src/main/java/com/fmi/domain/post/converter/PostConverter.java
+++ src/main/java/com/fmi/domain/post/converter/PostConverter.java
@@ -45,6 +45,7 @@ public class PostConverter {
                 .viewCnt(0)
                 .date(request.getDate())
                 .radius(request.getRadius())
+                .category(request.getCategory())
                 .temporarySave(request.isTemporarySave())
                 .createdAt(LocalDateTime.now())
                 .updatedAt(LocalDateTime.now())
@@ -138,6 +139,7 @@ public class PostConverter {
                 .imageUrls(imageUrls)
                 .itemStatus(post.getItemStatus())
                 .postType(post.getPostType())
+                .category(post.getCategory())
                 .build();
     }
 
