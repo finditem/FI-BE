f6637b396f0b1d4abeec671a6c6fac170bd3a86e
wook099 <codnrud99@naver.com>
2025-11-23 17:37:55 +0900

    chore: 사용하지않는 메서드 삭제

--- src/main/java/com/fmi/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/fmi/domain/comment/repository/CommentRepository.java
@@ -14,8 +14,6 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 
-    List<Comment> findByPostId(Long postid);
-    
     // 특정 사용자의 댓글 조회 (익명화 처리용)
     List<Comment> findByUser(User user);
 
