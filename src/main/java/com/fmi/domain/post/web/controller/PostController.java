5954b7367e61b984b07144745c922febbd4cd593
YooSejeong <nived3@naver.com>
2025-12-30 00:45:25 +0900

    docs: 스웨거 태그 추가

--- src/main/java/com/fmi/domain/post/web/controller/PostController.java
+++ src/main/java/com/fmi/domain/post/web/controller/PostController.java
@@ -10,6 +10,7 @@ import com.fmi.domain.post.web.dto.UpdatePostDto;
 import com.fmi.global.apiPayload.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
@@ -26,6 +27,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/post")
+@Tag(name = "Post", description = "게시글 관련 API")
 public class PostController {
 
     private final PostService postService;
