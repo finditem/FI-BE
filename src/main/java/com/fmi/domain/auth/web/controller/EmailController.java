4e32dcd4f2c20e1713f682bf469f561f94780e53
Nerdy <62089304+kkch1012@users.noreply.github.com>
2026-01-11 13:27:04 +0900

    refactor: EmailController API 경로 복수형으로 변경 (/auth/email -> /auths/email)

--- src/main/java/com/fmi/domain/auth/web/controller/EmailController.java
+++ src/main/java/com/fmi/domain/auth/web/controller/EmailController.java
@@ -16,7 +16,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/auth/email")
+@RequestMapping("/auths/email")
 @RequiredArgsConstructor
 @Tag(name = "Auth")
 public class EmailController {
