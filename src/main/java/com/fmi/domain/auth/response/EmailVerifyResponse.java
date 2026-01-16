cd2286cd67dca1ebbf4ed3637cead89db04baa3c
Nerdy <62089304+kkch1012@users.noreply.github.com>
2026-01-16 16:02:37 +0900

    docs: Auth 도메인 Response DTO에 Swagger 스키마 추가

--- src/main/java/com/fmi/domain/auth/response/EmailVerifyResponse.java
+++ src/main/java/com/fmi/domain/auth/response/EmailVerifyResponse.java
@@ -1,11 +1,14 @@
 package com.fmi.domain.auth.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Schema(description = "이메일 인증 응답")
 public class EmailVerifyResponse {
+    @Schema(description = "이메일 인증 여부", example = "true")
     private boolean verified;
 }
 
