c05687e2be90bdb82dd224aae29d893411538695
hyeonjaez <jhjh0022@naver.com>
2026-01-28 14:23:55 +0900

    feat: Post entity 수정 -> 칼럼 재배치 및 변수 변경, 생성자 create(), isNew() 메서드 추가

--- src/main/java/com/fmi/domain/post/data/Post.java
+++ src/main/java/com/fmi/domain/post/data/Post.java
@@ -1,34 +1,23 @@
 package com.fmi.domain.post.data;
 
 import com.fmi.domain.Enum.Category;
-import com.fmi.domain.Enum.Status;
-import com.fmi.domain.Enum.Type;
 import com.fmi.domain.auth.data.User;
-import com.fmi.domain.postfavorite.data.PostFavorite;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = true)
-    private User user;
 
     @Column(name = "title", nullable = false)
     private String title;
@@ -43,15 +32,15 @@ public class Post {
     private double longitude;
 
     @Column(name = "view_cnt")
-    private long viewCnt;
+    private long viewCount;
 
     @Column(name = "item_status")
     @Enumerated(EnumType.STRING)
-    private Status itemStatus;
+    private PostStatus postStatus;
 
     @Column(name = "post_type")
     @Enumerated(EnumType.STRING)
-    private Type postType;
+    private PostType postType;
 
     @Enumerated(EnumType.STRING)
     private Category category;
@@ -59,18 +48,14 @@ public class Post {
     @Column(name = "content", nullable = false)
     private String content;
 
-    @Column(name = "temporary_save",nullable = false)
+    @Column(name = "temporary_save", nullable = false)
     private boolean temporarySave;
 
     @Column(name = "date")
     private LocalDate date;
 
     @Column(name = "radius")
-    private double radius;
-
-    @Builder.Default
-    @Column(name="favorite_count")
-    private Integer favoriteCount = 0;
+    private Radius radius;
 
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
@@ -78,30 +63,31 @@ public class Post {
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE, orphanRemoval = true)
-    @OrderBy("displayOrder ASC")
-    @Builder.Default
-    private List<PostImage> images = new ArrayList<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
-    @Builder.Default
-    private List<PostFavorite> favorites = new ArrayList<>();
+    private Post(String title, String address, double latitude, double longitude, PostType postType, Category category, boolean temporarySave, LocalDate date, Radius radius, User user) {
+        this.title = title;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.postType = postType;
+        this.category = category;
+        this.temporarySave = temporarySave;
+        this.date = date;
+        this.radius = radius;
+        this.postStatus = PostStatus.SEARCHING;
+        this.createdAt = LocalDateTime.now();
+        this.user = user;
+        this.viewCount = 0;
+    }
 
-    @Column(name = "comment_count", nullable = false)
-    @Builder.Default
-    private Integer commentCount = 0;
+    public static Post create(String title, String address, double latitude, double longitude, PostType postType, Category category, boolean temporarySave, LocalDate date, Radius radius, User user) {
+        return new Post(title, address, latitude, longitude, postType, category, temporarySave, date, radius, user);
+    }
 
     public boolean isNew() {
-        return Duration.between(createdAt, LocalDateTime.now()).toHours() < 24;
-    }
-
-    public void increaseCommentCount() {
-        this.commentCount += 1;
-    }
-
-    public void decreaseCommentCount() {
-        if (this.commentCount > 0) {
-            this.commentCount -= 1;
-        }
+        return this.createdAt.isAfter(LocalDateTime.now().minusHours(24));
     }
 }
