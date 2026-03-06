f06306f6033c4997ab2aae3c9085a9e4bc956937
김찬호 <kkch9805@naver.com>
2026-03-06 16:06:06 +0900

    refactor: 비회원 문의 상세 조회에서 불필요한 댓글 조회 제거

--- src/main/java/com/fmi/domain/admin/service/AdminService.java
+++ src/main/java/com/fmi/domain/admin/service/AdminService.java
@@ -215,7 +215,7 @@ public class AdminService {
                 .build();
     }
 
-    public InquiryDetailDTO getGuestInquiryDetail(Long inquiryId, UserDetails userDetails) {
+    public InquiryDetailDTO getGuestInquiryDetail(Long inquiryId) {
         Inquiry inquiry = inquiryRepository.findById(inquiryId)
                 .orElseThrow(() -> new GeneralException(ErrorStatus._INQUIRY_NOT_FOUND));
 
@@ -223,9 +223,7 @@ public class AdminService {
             throw new GeneralException(ErrorStatus._INQUIRY_NOT_FOUND);
         }
 
-        List<InquiryCommentResponse> comments =
-                inquiryCommentService.getCommentsForDetail(inquiry.getId(), userDetails);
-        return inquiryConverter.toDetailDTO(inquiry, comments);
+        return inquiryConverter.toDetailDTO(inquiry);
     }
 
     @Transactional
