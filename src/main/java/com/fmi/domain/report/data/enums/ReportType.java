aeba55c43e9bb42f784fccfe6b4148abc4766c75
김찬호 <kkch9805@naver.com>
2026-03-11 17:51:43 +0900

    feat: 신고 유형(ReportType) 서비스 맞춤 값으로 변경

--- src/main/java/com/fmi/domain/report/data/enums/ReportType.java
+++ src/main/java/com/fmi/domain/report/data/enums/ReportType.java
@@ -4,16 +4,13 @@ import lombok.Getter;
 
 @Getter
 public enum ReportType {
-    FRAUD("허위/사기"),
-    SPAM("스팸/광고"),
-    INAPPROPRIATE("부적절한 콘텐츠"),
-    ABUSE("욕설/비방"),
-    HARASSMENT("괴롭힘"),
-    VIOLENCE("폭력적 콘텐츠"),
-    ADULT("음란물"),
-    PRIVACY("개인정보 노출"),
-    COPYRIGHT("저작권 침해"),
-    ETC("기타");
+    IRRELEVANT_CONTENT("실제 분실물/습득물이 아닌 내용이에요."),
+    DUPLICATE("동일한 내용이 여러 번 올라왔어요."),
+    SPAM("분실물과 무관한 홍보성 게시글이에요."),
+    OFFENSIVE_LANGUAGE("채팅 또는 댓글에 모욕적 표현이 있어요."),
+    EXTORTION("물건 반환을 빌미로 금전 요구가 있어요."),
+    FALSE_CLAIM("실제 주인이 아닌 사람이 소유를 주장해요."),
+    ETC("위 항목 외의 다른 문제를 신고해요.");
 
     private final String description;
 
