cf50bd5cf81edc658ba9eee9a03c50a6bb6972f2
YooSejeong <nived3@naver.com>
2026-08-11 20:46:32 +0900

    refactor: 사용되지 않는 @Slf4j 제거

--- src/main/java/com/fmi/domain/inquiry/listener/InquiryEventListener.java
+++ src/main/java/com/fmi/domain/inquiry/listener/InquiryEventListener.java
@@ -3,14 +3,12 @@ package com.fmi.domain.inquiry.listener;
 import com.fmi.domain.inquiry.event.InquiryEvent;
 import com.fmi.global.service.SlackService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class InquiryEventListener {
 
     private final SlackService slackService;
