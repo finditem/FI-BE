c981c1f927ea7a68150349875cb2e5bd429f69cb
박상혁 <pshsh910@gmail.com>
2026-09-15 17:48:48 +0900

    perf: 팝업 종료 시각 엔티티 매핑 추가

--- src/main/java/com/fmi/domain/place/data/PlaceOperationPeriod.java
+++ src/main/java/com/fmi/domain/place/data/PlaceOperationPeriod.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -33,6 +34,9 @@ public class PlaceOperationPeriod extends BaseEntity {
 
     @Column(name = "end_date", nullable = false)
     private LocalDate endDate;
+
+    @Column(name = "closing_at")
+    private LocalDateTime closingAt;
 
     @Builder
     private PlaceOperationPeriod(LocalDate startDate, LocalDate endDate) {
